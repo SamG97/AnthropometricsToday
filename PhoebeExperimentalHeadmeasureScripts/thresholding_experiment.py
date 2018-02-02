0a75eae61de7a89b773a869061bfc432e413ff02
PhoebeMay <phoebenichols@gmail.com>
2018-02-02 23:38:15 +0000

    wrote function to measure width of head using thresholding

--- PhoebeExperimentalHeadmeasureScripts/thresholding_experiment.py
+++ PhoebeExperimentalHeadmeasureScripts/thresholding_experiment.py
@@ -3,18 +3,47 @@ from skimage.filters import threshold_mean
 from skimage.io import imsave
 from skimage.viewer import ImageViewer
 
-base_location = "/Users/phoebenichols/AnthropometricsToday/PhoebeExperimentalHeadmeasureScripts/images/"
+cmPerPixel = 0.0458
 
-def thresholdImage(imagepath):
-    color_input = data.load(imagepath)
-    image = color.rgb2grey(color_input)
-    # Threshold is mean intensity
-    thresh = threshold_mean(image)
-    binary = image > thresh
+base_location = "/Users/phoebenichols/AnthropometricsToday/" \
+                "PhoebeExperimentalHeadmeasureScripts/images/"
+
+
+def saveThresholdImage(imagepath):
+    binary = getThresholdImage(imagepath)
     print(imagepath)
     location = 'output/thresholding/' + imagepath[
                                         len(base_location):]
     imsave(location, img_as_uint(binary))
 
-thresholdImage("/Users/phoebenichols/AnthropometricsToday/PhoebeExperimentalHeadmeasureScripts/images/phoebe_profile_centered.jpg")
 
+def getThresholdImage(imagepath):
+    color_input = data.load(imagepath)
+    image = color.rgb2grey(color_input)
+    # Threshold is mean intensity
+    thresh = threshold_mean(image)
+    binary = image > thresh
+    return binary
+
+#TODO: discuss edge cases, for example where black at edge of image
+def getHeadLength(imagepath):
+    binary = getThresholdImage(imagepath)
+    mid_point = len(binary) // 2
+    row = binary[mid_point]
+
+    most_consecutive_falses = 0
+    current_consecutive_falses = 0
+
+    for i in range(len(row)):
+        if row[i]:
+            most_consecutive_falses = max(most_consecutive_falses,
+                                          current_consecutive_falses)
+            current_consecutive_falses = 0
+        else:
+            current_consecutive_falses += 1
+
+    return most_consecutive_falses*cmPerPixel
+
+print(getHeadLength("/Users/phoebenichols/AnthropometricsToday/"
+              "PhoebeExperimentalHeadmeasureScripts/"
+              "images/phoebe_profile_centered.jpg"))
