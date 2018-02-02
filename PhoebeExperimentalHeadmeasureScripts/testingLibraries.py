551b47cb6aa20a9df50ac24b928f5c25b0914fab
PhoebeMay <phoebenichols@gmail.com>
2018-02-02 20:38:56 +0000

    have working face and profile detection

--- PhoebeExperimentalHeadmeasureScripts/testingLibraries.py
+++ PhoebeExperimentalHeadmeasureScripts/testingLibraries.py
@@ -2,17 +2,25 @@ import numpy as np
 import face_recognition
 import cv2
 
-phoebe_face = "images/phoebe_face_noglasses.jpg"
-phoebe_profile = "images/phoebe_profile_right.jpg"
-rando_profile = "images/rando_profileface_left.jpg"
+all_faces = ["images/phoebe_face_noglasses.jpg",
+              "images/phoebe_face_glasses.jpg"]
 
-def test_face_recognition():
+all_profiles = ["images/phoebe_profile_right.jpg",
+                "images/phoebe_profile_left.jpg",
+                "images/rando_profileface_left.jpg",
+                "images/rando_profileface_right.jpg"]
+
+base_location = "images/"
+
+def test_face_recognition(imagepath):
     print("Testing face_recognition module")
 
     image = face_recognition \
-        .load_image_file(phoebe_face)
+        .load_image_file(imagepath)
 
     face_landmarks_list = face_recognition.face_landmarks(image)
+    if (len(face_landmarks_list) == 0):
+        print("No face detected")
 
     print(len(face_landmarks_list))
     print("Can classify on :")
@@ -21,37 +29,48 @@ def test_face_recognition():
     print("For the right eye, the given data is:")
     print(face_landmarks_list[0].get('right_eye'))
 
-
-def test_opencv_faces():
-
+def test_opencv_faces(imagepath):
     face_cascade = cv2.CascadeClassifier('openCVCascades/haarcascade_frontalface_default.xml')
 
-    img = cv2.imread(phoebe_face)
+    img = cv2.imread(imagepath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print(faces)
+    print(type(faces))
+    if faces == ():
+            print("No face detected")
+            return
     print(faces[0])
     for (x, y, w, h) in faces:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    cv2.imwrite('output/opencv_for_facewidth.jpg', img)
+    location = 'output/opencv_for_face_width/'+imagepath[len(base_location):]
+    cv2.imwrite(location, img)
 
-def test_opencv_profiles():
+def test_opencv_profiles(imagepath):
     profile_cascade = cv2.CascadeClassifier(
         'openCVCascades/haarcascade_profileface.xml')
 
-    img = cv2.imread(rando_profile)
+    img = cv2.imread(imagepath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     profiles = profile_cascade.detectMultiScale(gray, 1.3, 5)
+    if profiles == ():
+            print("No profiles detected")
+            return
     print(profiles[0])
     for (x, y, w, h) in profiles:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    cv2.imwrite('output/opencv_for_profiles.jpg', img)
+    location = 'output/opencv_for_profiles/' + imagepath[len(base_location):]
+    cv2.imwrite(location, img)
 
 
 # test_face_recognition()
-# test_opencv_faces()
-test_opencv_profiles()
+for face in all_faces:
+    test_opencv_faces(face)
+
+for profile in all_profiles:
+    test_opencv_faces(profile)
 
 # Notes:
 #
