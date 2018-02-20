e7002fdc99c780e408aaaa6bb77129488ec44195
rgssad <procedurehhl@hotmail.com>
2018-02-20 13:04:11 +0000

    Modified table for generation and front end

--- dummy database/table_generated.sql
+++ dummy database/table_generated.sql
@@ -3,22 +3,17 @@ CREATE TABLE public.generatedpeople
    "DM_month" integer,
    "DM_day" integer,
    "DM_year" integer,
+   "Age" integer,
    "Name" character varying(100), -- in case if any name like Ovuvuevuevue Onyetenyevwe Ugwemubwem Ossas will appear
    "College" character varying(20),
-   "Age" integer,
-   "Birthplace1" character varying(100), -- in case if any place like Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu will appear
-   "Birthplace2" character varying(100),
-   "Father_origin" character varying(100),
-   "Mother_origin" character varying(100),
-   "Skin" character varying(20),
-   "Hair_colour" character varying(20),
-   "Eyes" character varying(20), -- light, medium, or dark
-   "Hair_type" character varying(20),
-   "Head_length" integer, -- all of the following measurement should be stored in mm
-   "Head_breadth" integer,
-   "Head_height" integer,
+   "Sex" character varying(20),
+   "Skin_Colour" character varying(20),
+   "Eye_Colour" character varying(20),
+   "Hair_Colour" character varying(20),
+   -- all of the following measurement should be stored in mm
+   "Head_length" integer, 
    "Face_breadth" integer,
-   "Face_iobreadth" integer,
-   id serial NOT NULL,
+   "Face_iobreadth" integer, --interocular length
+   id serial NOT NULL, --no need to generate as this field will be automatically generated upon import
    CONSTRAINT generatedpeoplestudentkey PRIMARY KEY (id)
 )
