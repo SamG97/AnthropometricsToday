26dfc4f44ffdfb61097dda42abfd0191e1b8d446
SamG97 <samgooch97@googlemail.com>
2018-02-21 17:45:06 +0000

    Added report page fields for data about twin

--- front-end/anthropometrics-today/src/report/report.jsx
+++ front-end/anthropometrics-today/src/report/report.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReportComponent from './reportComponent';
+import QRCode from 'qrcode.react';
 import { map } from 'lodash';
-import config from '../utility/config';
 import capitalise from '../utility/capitalise';
+import config from '../utility/config';
+import Spinner from '../utility/loading';
+import { ComparisonReportComponent, TwinReportComponent } from './reportComponents';
+import { MalformedMeasurements, ServerError } from '../errors';
 import { getHistoricData } from '../requests/requestWrappers';
 import { uncompress } from '../utility/urlCompress';
-import { MalformedMeasurements, ServerError } from '../errors';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import './report.css';
-import Spinner from '../utility/loading';
 
 export default class Report extends React.Component {
     constructor(props) {
@@ -26,8 +27,6 @@ export default class Report extends React.Component {
         } else {
             this.userMeasurements = null;
         }
-
-        console.log(this.userMeasurements);
 
         this.state = {
             isLoading: true,
@@ -51,14 +50,43 @@ export default class Report extends React.Component {
         })
     };
 
-    generateFieldComponent = (field) => {
+    generateTwinComponents = (field) => {
+        if (field === 'dob') {
+            const dob = this.state.twinMeasurements['dobd'] + '/' + this.state.twinMeasurements['dobm'] + '/' +
+                this.state.twinMeasurements['doby'];
+            return (
+                <TwinReportComponent
+                    key={field}
+                    fieldName={'Date of Birth'}
+                    userValue={this.userMeasurements[field]}
+                    twinValue={dob}
+                />
+            );
+        }
+
+        const fieldName = (field === 'hair' || field === 'eye') ? (field + ' colour') : field;
+
         return (
-            <ReportComponent
+            <TwinReportComponent
                 key={field}
-                fieldName={capitalise(field)}
+                fieldName={capitalise(fieldName)}
                 userValue={this.userMeasurements[field]}
                 twinValue={this.state.twinMeasurements[field]}
-             />
+            />
+        );
+    };
+
+    generateComparisonComponent = (field) => {
+        const isMeasurement = field !== 'name';
+        const fieldName = isMeasurement ? (field + ' width') : field;
+
+        return (
+            <ComparisonReportComponent
+                key={field}
+                fieldName={capitalise(fieldName)}
+                userValue={this.userMeasurements[field] + (isMeasurement ? ' cm' : '')}
+                twinValue={this.state.twinMeasurements[field] + (isMeasurement ? ' cm' : '')}
+            />
         );
     };
 
@@ -70,45 +98,67 @@ export default class Report extends React.Component {
         if (this.state.isLoading) {
             return (
                 <div className="loading-container">
-                    <Spinner />
+                    <Spinner/>
                 </div>
             );
         }
 
         if (this.state.loadFailed) {
             return (
-                <ServerError />
+                <ServerError/>
             );
         }
 
         if (!this.userMeasurements) {
             return (
-                <MalformedMeasurements />
+                <MalformedMeasurements/>
             );
         }
 
-        const reportBody = (<div>{map(config.fieldOrdering, (field) => this.generateFieldComponent(field))}</div>);
+        const linkAddress = config.webBaseUrl + '/report/' + this.props.match.params.twin + '/' +
+            this.props.match.params.user;
+
+        const twinBody = (<div>{map(config.twinOrdering, (field) => this.generateTwinComponents(field))}</div>);
+
+        const reportBody = (<div>{map(config.fieldOrdering, (field) => this.generateComparisonComponent(field))}</div>);
 
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="number-container">
-                        <div className="number-label">
-                            No.
+            <div className="outer-container">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="number-container">
+                                <div className="number-label">
+                                    No.
+                                </div>
+                                <div className="report-number">
+                                    {Math.floor(Math.random() * 9000 + 1000)}
+                                </div>
+                            </div>
                         </div>
-                        <div className="report-number">
-                            {Math.floor(Math.random() * 9000 + 1000)}
+                        <div className="col-6">
+                            <QRCode className="qr-code" value={linkAddress} bgColor="#CAC3BB"/>
                         </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="title col-12">
-                        ANTHROPOMETRICS TODAY
+                    <div className="row">
+                        <div className="intro col-12">
+                            Your historical twin is {this.twin['name']}! <br/>
+                            Find out more about them and how you compare in your report below!
+                        </div>
                     </div>
-                </div>
-                {reportBody}
-                <div className="row">
-                    <div className="footer" />
+                    <div className="row">
+                        <div className="title col-12">
+                            ANTHROPOMETRICS TODAY
+                        </div>
+                    </div>
+                    {twinBody}
+                    <div className="row">
+                        <div className="divider"/>
+                    </div>
+                    {reportBody}
+                    <div className="row">
+                        <div className="divider"/>
+                    </div>
                 </div>
             </div>
         );
