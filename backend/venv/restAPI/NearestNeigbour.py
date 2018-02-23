a01abd9d94c2e5e6d4d785ab98ac2871aff0c702
Tlee43 <tomlee4348@gmail.com>
2018-02-23 15:47:00 +0000

    added new database code to venv

--- backend/venv/restAPI/NearestNeigbour.py
+++ backend/venv/restAPI/NearestNeigbour.py
@@ -1,19 +1,19 @@
 import numpy as np
-#from restAPI.DataBaseScript import getData
+from restAPI.DataBaseScript import getData
 
-#def calcCovarienceMatrix():
-#    studentList = getData()
-#    students = [{studentList.get()[j][i][0]: studentList.getall()[j][i][1] for i in range(len(studentList.getall()[j]))}
-#                for j in range(len(studentList.getall()))]
-#    dists = np.array([np.array([students[i]['Head_length'], students[i]['Face_breadth'], students[i]['Face_iobreadth']]) for i in
-#             range(len(students))])
-#    return np.cov(dists)
+def calcCovarienceMatrix():
+    studentList = getData()
+    students = [{studentList.get()[j][i][0]: studentList.getall()[j][i][1] for i in range(len(studentList.getall()[j]))}
+                for j in range(len(studentList.getall()))]
+    dists = np.array([np.array([students[i]['Head_length'], students[i]['Face_breadth'], students[i]['Face_iobreadth']]) for i in
+             range(len(students))])
+    return np.cov(dists)
 
-#elipseMatrix = np.linalg.inv(calcCovarienceMatrix())
+elipseMatrix = np.linalg.inv(calcCovarienceMatrix())
 
-#def calcDist(a, b):
-#    ab = b-a
-#    return np.sqrt(np.linalg.multi_dot(np.linalg.multi_dot(np.transpose(ab),elipseMatrix), ab))
+def calcDist(a, b):
+    ab = b-a
+    return np.sqrt(np.dot(np.dot(np.transpose(ab),elipseMatrix), ab))
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
